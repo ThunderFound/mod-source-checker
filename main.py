6145e48bb6dd8d061205ca576ce153b6d911bde7
ThunderFound <90287659+ThunderFound@users.noreply.github.com>
2026-02-26 17:24:27 +0200

    updated the fingerprint calculation function, made it 3x faster, but it still takes 10s:sob:

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import hashlib
 import os
 import requests
 import json
+import time
+import struct
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -13,45 +15,37 @@ def get_sha1(filename):
             h.update(chunk)
     return h.hexdigest()
 
-def get_curseforge_fingerprint(filename: str):
+def get_curseforge_fingerprint(filename: str) -> int:
     multiplex = 1540483477
     
     with open(filename, 'rb') as f:
         data = f.read()
 
-    ignored_bytes = {9, 10, 13, 32}
-    
-    normalized = [b for b in data if b not in ignored_bytes]
+    normalized = data.translate(None, b'\x09\x0a\x0d\x20')
     length = len(normalized)
     
     if length == 0:
         return 0
 
-    hash_val = 1 ^ length 
+    hash_val = 1 ^ length
     
-    buffer = 0
-    shift = 0
+    num_chunks = length // 4
+    remainder = length % 4
     
-    def to_uint32(n):
-        return n & 0xFFFFFFFF
-
-    for b in normalized:
-        buffer |= (b << shift)
-        shift += 8
+    if num_chunks > 0:
+        chunk_data = normalized[:num_chunks * 4]
         
-        if shift == 32:
-            k = to_uint32(buffer * multiplex)
-            k = to_uint32((k ^ (k >> 24)) * multiplex)
-            hash_val = to_uint32(to_uint32(hash_val * multiplex) ^ k)
+        for (k,) in struct.iter_unpack('<I', chunk_data):
+            k = (k * multiplex) & 0xFFFFFFFF
+            k = ((k ^ (k >> 24)) * multiplex) & 0xFFFFFFFF
+            hash_val = ((hash_val * multiplex) ^ k) & 0xFFFFFFFF
             
-            buffer = 0
-            shift = 0
-            
-    if shift > 0:
-        hash_val = to_uint32((hash_val ^ buffer) * multiplex)
+    if remainder > 0:
+        buffer = int.from_bytes(normalized[-remainder:], byteorder='little')
+        hash_val = ((hash_val ^ buffer) * multiplex) & 0xFFFFFFFF
         
-    hash_val = to_uint32((hash_val ^ (hash_val >> 13)) * multiplex)
-    hash_val = to_uint32(hash_val ^ (hash_val >> 15))
+    hash_val = ((hash_val ^ (hash_val >> 13)) * multiplex) & 0xFFFFFFFF
+    hash_val = (hash_val ^ (hash_val >> 15)) & 0xFFFFFFFF
     
     return hash_val
     
@@ -61,11 +55,14 @@ def check_curseforge(session, filenames):
         'Accept': 'application/json',
         'x-api-key': os.getenv("CURSEFORGE_API_KEY")
     }
-
+    start = time.time()
     fingerprints = {filename: get_curseforge_fingerprint(filename) for filename in filenames}
+    print(f"calculating fingerprints took {time.time() - start}s")
+    start = time.time()
     response = session.post('https://api.curseforge.com/v1/fingerprints', headers=headers, json={
         "fingerprints": list(fingerprints.values())
     })
+    print(f"curseforge took {time.time() - start}s")
 
     data = response.json()
     matches = data["data"]["exactFingerprints"]
@@ -83,10 +80,12 @@ def check_curseforge(session, filenames):
 
 def check_modrinth(session, filenames):
     hashes = {filename: get_sha1(filename) for filename in filenames}
+    start = time.time()
     response = session.post("https://api.modrinth.com/v2/version_files", json={
         "hashes": list(hashes.values()),
         "algorithm": "sha1"
     })
+    print(f"modrinth took {time.time() - start}s")
 
     data = response.json()
     # print(json.dumps(data, indent=4, sort_keys=True))
@@ -112,7 +111,7 @@ BLUE = "\033[34m"
 session = requests.Session()
 
 def check(session, directory="mods"):
-    #TODO: add progressbar and maybe other websites
+    #TODO: add progressbar
     filepaths = [os.path.join(directory, filename) for filename in os.listdir(directory) if os.path.isfile(os.path.join(directory, filename))]
     modrinth_result = check_modrinth(session, filepaths)
     curseforge_result = check_curseforge(session, filepaths)
